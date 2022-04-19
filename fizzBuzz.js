ab5982b61b50ccca59152b709b5097267b2af528
bdji <sadate1992@live.fr>
2022-04-18 19:52:49 -0700

    second Commit

---
+++ fizzBuzz.js
@@ -0,0 +1,12 @@
+let i;
+for (i=1; i<=100; i++){
+    if(i%15==0){
+        console.log("fizzbuzz"); 
+    }
+    if (i%3==0){
+        console.log("fizz");
+    }
+    if(i%5==0){
+        console.log("buzz");
+    }
+}
